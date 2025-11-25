6709fde5f8a99f2e83ae452268f5fcf725fcdcff
baraksu <baraksu@gmail.com>
2025-11-25 10:45:46 +0200

    first version.

---
+++ Encryption.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Encryption here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Encryption
+{
+    public static void main(String[] args){
+        
+        System.out.println("Welcome to the Encryption / Decryption Program");
+        
+    }
+}

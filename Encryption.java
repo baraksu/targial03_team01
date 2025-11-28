df35e14b20ace0e5d92f7a011d0b593f5d57a355
Eitan1245 <eitan.azran1@gmail.com>
2025-11-28 10:55:08 +0200

    Update Encryption.java did only the starting build

--- Encryption.java
+++ Encryption.java
@@ -1,15 +1,17 @@
-/**
- * Write a description of class Encryption here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
+import java.util.*;
 public class Encryption
 {
     public static void main(String[] args){
-        
-        System.out.println("Welcome to the Encryption / Decryption Program");
-        
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Welcome to the Encryption / Decryption Programn\n Enter 1 for Encrypt | 2 for Decrypt");
+        int choose = reader.nextInt();
+        if(choose == 1)
+            System.out.println("goat");
+        else if(choose == 2)
+        System.out.println("goat");
+        else 
+            System.out.println("ERROR - no such option");
+    }
     }
     public static int add (int a, int b){
         return a+b ;

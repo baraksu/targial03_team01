16e3a8b28ed9f0741f64b53f65efb139be4a0c77
Eitan1245 <eitan.azran1@gmail.com>
2025-12-02 16:05:20 +0200

    Update EncryptionTester.java

--- EncryptionTester.java
+++ EncryptionTester.java
@@ -10,11 +10,13 @@ import org.junit.Test;
  */
 public class EncryptionTester
 {
+        
+    
     @Test
     public void test1(){
         
-        int result = Encryption.add(2,3);
-        assertEquals(5,result);
+        String result = Encryption.replace("0bc");
+        assertEquals("obc",result);
         
     }
     @Test
@@ -46,16 +48,30 @@ public class EncryptionTester
         
     }
     @Test
-    public void testAdd(){
+    public void test6(){
         
         String result = Encryption.replace("abu");
         assertEquals("@b&",result);
         
     }
     @Test 
-        public void test2(){
+        public void test7(){
             String result = Encryption.replace("onion");
             assertEquals("0n10n",result);
         }
-    
+    @Test 
+        public void test8(){
+            String result = Encryption.replace("eitan");
+            assertEquals("#1t@n",result);
+        }
+    @Test 
+        public void test9(){
+            String result = Encryption.replace("noam");
+            assertEquals("n0@m",result);
+        }
+    @Test 
+        public void test10(){
+            String result = Encryption.replace("qreuba");
+            assertEquals("qr#&b@",result);
+        }
 }
